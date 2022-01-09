693b322f7f09de04d6da01c8efd60523d8d37f4f
SiggeEd <64078291+SiggeEd@users.noreply.github.com>
2022-01-09 12:37:33 +0100

    ff

--- Includes/Component.h
+++ Includes/Component.h
@@ -9,12 +9,12 @@ class Component
 {
 public:
     virtual void mouseMove(int x);
-    virtual draw() const = 0;
-    SLD_Rect getRect() const {return rect;}
+    virtual void draw() const = 0;
+    SDL_Rect getRect() const {return rect;}
     virtual void tick() = 0;
 
 protected:
-    Component(int x, int y, int w, int h) : rect(x,y,w,h){}
+    Component(int x, int y, int w, int h) : rect{ x,y,w,h } {}
     SDL_Rect rect;
 };
 
